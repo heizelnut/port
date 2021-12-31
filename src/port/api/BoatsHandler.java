02e0d03f2c83a87c59dcd7c4541450f03db6cd78
heizelnut <git@heiz.cc>
2021-12-31 18:48:13 +0000

    add api interface & improve build system

---
+++ src/port/api/BoatsHandler.java
@@ -0,0 +1,28 @@
+package port.api;
+
+import java.util.*;
+import java.io.*;
+import com.sun.net.httpserver.*;
+import org.json.JSONObject;
+
+public class BoatsHandler implements HttpHandler {
+	public void handle(HttpExchange t) throws IOException {
+		InputStream is = t.getRequestBody();
+		
+		System.out.println(t.getRequestURI());
+
+		Headers reqh = t.getRequestHeaders();
+		System.out.println(reqh.getFirst("User-Agent"));
+
+		Headers resh = t.getResponseHeaders();
+		resh.set("Content-Type", "application/json");
+		JSONObject obj = new JSONObject();
+		obj.put("msg", "boats service");
+
+		String response = obj.toString();
+		t.sendResponseHeaders(200, response.length());
+		OutputStream os = t.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+   }
+}
