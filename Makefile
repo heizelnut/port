87a0a52b3f17847d877348642e7b8209f865e1d3
heizelnut <git@heiz.cc>
2022-01-06 21:28:30 +0000

    update directory tree

--- Makefile
+++ Makefile
@@ -7,9 +7,9 @@ class:
 cli: class
 	./tom run
 
-serve: class
-	./tom run serve
-
 jar: class
 	rm $(PKGNAME).jar
 	./tom bundle
+
+serve: jar
+	java -jar $(PKGNAME).jar serve
--- public/port-ui.js
+++ public/port-ui.js
@@ -1,9 +1,12 @@
 const port = new Port();
 const dashBoard = document.getElementsByTagName("main")[0];
 const briefBoatCard = document.getElementById("brief-boat");
+const nothing = document.getElementById("nothing");
 
 port.getAll().then(boats => {
 	for (let boat of boats) {
+		nothing.style.display = "none";
+
 		let card = briefBoatCard.content.cloneNode(true);
 		let templateHTML = card.firstElementChild.innerHTML;
 		templateHTML = templateHTML.replace("%name%", boat.name);
